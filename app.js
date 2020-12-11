1bd28d459212f16ca817ddfaaa4e2a231a724892
Kand-h <hshuttleworth0@gmail.com>
2020-12-11 10:34:27 +0000

    Add api

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const { Message } = require('./db/models/message.model');
 app.use(bodyParser.json());
 
 // Create link to Angular build directory
-var distDir = __dirname + "/public/";
+var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
 // cors headers middleware
